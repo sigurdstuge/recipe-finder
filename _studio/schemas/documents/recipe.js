c3e52a84a561d3ae6a4300ed5313ce643a383b73
sigurdstuge <sigurd.stuge@gmail.com>
2023-04-27 11:22:40 +0200

    Created schemas for category and recipe

--- _studio/schemas/documents/recipe.js
+++ _studio/schemas/documents/recipe.js
@@ -1,8 +1,73 @@
 export default {
-	title: 'Category',
-	name: 'category',
+	title: 'Recipe',
+	name: 'recipe',
 	type: 'document',
 	fields: [
-		
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image',
+		},
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Difficulty',
+			name: 'difficulty',
+			type: 'string',
+		},
+		{
+			title: 'Ingredients',
+			name: 'ingredients',
+			type: 'array',
+			of: [
+				{
+					type: 'object',
+					fields:[
+						{
+							title: 'Name',
+							name: 'name',
+							type: 'string',
+						},
+						{
+							title: 'Quantity',
+							name: 'quantity',
+							type: 'number',
+						},
+						{
+							title: 'Unit',
+							name: 'unit',
+							type: 'string',
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Description',
+			name: 'description',
+			type: 'text',
+		},
+		{
+			title: 'Rating',
+			name: 'rating',
+			type: 'string',
+			options: {
+							list: [
+								'1',
+								'2',
+								'3',
+								'4',
+							]
+			}
+		},
+		{
+			title: 'Category',
+			name: 'category',
+			type: 'reference',
+			to: [{ type: 'category' }]
+		},	
 	]
 }
